97fafde2f6a53417179bdad4a1a109c165f68f0f
natewilcox@gmail.com <natewilcox@gmail.com>
2023-12-10 14:46:20 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# colyseus-nathan
+# Colyseus Nathan Repository
+
+Nathan's extensions for Colyseus
+
+## Getting Started
+
+To use them in your Colyseus server, install the package using npm:
+
+```bash
+npm install @natewilcox/colyseus-nathan
---
+++ src/services/ClientService.ts
@@ -0,0 +1,36 @@
+import { Client, Room } from "colyseus";
+
+//message types have to include SendMessage
+type EnumWithSendMessage<T> = T & { SendMessage: 0 };
+
+export class ClientService<M> {
+
+    private room: Room;
+    private clientHandlers = new Map<number, (client: Client, msg: any) => void>();
+    private msgs = { SendMessage: 0 } as EnumWithSendMessage<M>;
+
+    constructor(room: Room) {
+        this.room = room;
+
+        this.room.onMessage(this.msgs.SendMessage, (client, data) => {
+    
+            const handler = this.clientHandlers.get(data.type);
+       
+            if(handler) {
+                handler(client, data);
+            }
+        });
+
+    }
+
+    send(msgType: number, data?: any) {
+        this.room.broadcast(this.msgs.SendMessage, {
+            type: msgType,
+            ...data,
+        });
+    }
+
+    on(msgType: number, cb: (client: Client, data: any) => void, context?: any) {
+        this.clientHandlers.set(msgType, cb);
+    }
+}
